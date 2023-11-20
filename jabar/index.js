cf7dce054fa822d02cf91c776019f5ae23853435
ismaQayyam <151512255+ismaQayyam@users.noreply.github.com>
2023-11-20 13:46:19 +0500

    Add files via upload
    this is my javascript file

---
+++ jabar/index.js
@@ -0,0 +1,13 @@
+
+let number = [22,33,44,55,66,77,88,99];
+
+// if(number%2==0)
+// {
+//     console.log(number)
+// }
+for(let i =0; i<number.length; i++){
+if(i%2==0){
+    console.log(i)
+}
+}
+
